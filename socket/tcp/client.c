fb1ee63d00760c7404d6ab4950de0451747c1f23
wusuopubupt <wdxbupt2009@gmail.com>
2015-10-25 22:47:32 +0800

    added Makefile

--- socket/tcp/client.c
+++ socket/tcp/client.c
@@ -1,32 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
-#include <netinet/in.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <arpa/inet.h>
 
-const SERV_PORT=1158;
+// 注：以上头文件的添加查阅linux man page得到(http://linux.die.net/man/3/htons)
+
+const char * SERV_ADDR = "127.0.0.1";
+const int SERV_PORT = 1158;
 
 int main(int argc, char *argv[]) {
     int sockfd;
     struct sockaddr_in servaddr;
+
+    int port = SERV_PORT;
+    if(argc == 2) {
+        port = atoi(argv[1]);
+    }
 
     // socket
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&servaddr, 0, sizeof(servaddr));    
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(SERV_PORT);
+    servaddr.sin_addr.s_addr = inet_addr(SERV_ADDR);
 
     // connect
-    connect(sockfd, (struct servaddr*)&servaddr, sizeof(servaddr));
+    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
     
     char buff[1024];
+    memset(buff, '\0', sizeof(buff));
     while(scanf("%1023s", buff) != EOF) {
         // write 
+        printf("in buff: \"%s\"\n", buff);
         write(sockfd, buff, strlen(buff));
+        memset(buff, '\0', sizeof(buff));
         // read
         read(sockfd, buff, strlen(buff));
         printf("return from server: \"%s\"\n", buff);
-            
+        memset(buff, '\0', sizeof(buff));
     }
     // close
     close(sockfd);
